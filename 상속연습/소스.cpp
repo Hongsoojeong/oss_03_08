dea2967b20c6a7a62093b4779d700c33caddee76
Hongsoojeong <my_youth99@naver.com>
2021-03-08 10:51:31 +0900

    03-08 오픈소스 실습

---
+++ 상속연습/소스.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+
+class Animal
+{
+public: 
+	virtual void speak() {
+		cout << "Animal speak()" << endl;
+	}
+};
+
+class Dog : public Animal
+{
+public:
+	int age;
+	void speak() {
+		cout << "港港" <<endl;
+	}
+};
+
+class Cat : public Animal {
+public:
+	int age;
+	void speak() {
+		cout << "具克" << endl;
+	}
+};
+
+int main() {
+	Dog d;
+	Animal& a1 = d;
+	a1.speak();
+
+	Cat c;
+	Animal& a2 = c;
+	a2.speak();
+
+
+	return 0;
+}
